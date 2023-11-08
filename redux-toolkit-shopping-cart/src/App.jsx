fe019fb9e3bc566dac1f0175cb40ba77a011c67c
olwpad <olwpad@gmail.com>
2023-11-08 10:53:41 -0500

    se agregó isLoading

--- redux-toolkit-shopping-cart/src/App.jsx
+++ redux-toolkit-shopping-cart/src/App.jsx
@@ -1,12 +1,17 @@
-import CartContainer from "./components/cart/CartContainer";
-import NavBar from "./components/navbar/NavBar";
-import Modal from "./components/modal";
-import { useDispatch,useSelector} from "react-redux";
 import { useEffect } from "react";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { useSelector, useDispatch } from "react-redux";
+
+// Store
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+
+// Componentes
+import CartContainer from "./components/cart/CartContainer";
+import Loading from "./components/Loading";
+import Modal from "./components/Modal";
+import NavBar from "./components/navbar/NavBar";
 //import CartContainer from "./components/cart/CartContainer";
 const App = () => {
-  const {cartItems}=useSelector((store)=>store.cart);
+  const {cartItems,isLoading}=useSelector((store)=>store.cart);
   const {isOpen}=useSelector(store=>store.modal)
   const dispatch=useDispatch();
 
@@ -14,7 +19,13 @@ const App = () => {
     dispatch(calculateTotals())
   }, [cartItems,dispatch])
   
+useEffect(() => {
+  dispatch(getCartItems())
+}, [dispatch])
 
+
+
+if(isLoading) return<Loading/>
   return (
     <>
       {isOpen && <Modal/>}
--- redux-toolkit-shopping-cart/src/features/cart/cartSlice.js
+++ redux-toolkit-shopping-cart/src/features/cart/cartSlice.js
@@ -1,11 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../data/cartData";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//import cartItems from "../../data/cartData";
 
 const initialState = {
-  cartItems,
-  amount: 3,
-  total: 60,
+  cartItems: [],
+  amount: 0,
+  total: 0,
+  isLoading: true,
 };
+
+const url = "http://localhost:3000/items";
+
+// Middleware
+// Estados de la petición asíncrona (promises)
+// 1. Pending
+// 2. Fulfilled (éxito)
+// 3. Rejected
+export const getCartItems = createAsyncThunk("cart/getCartItems", async () => {
+  try {
+    const result = await axios.get(url);
+    return result.data;
+  } catch (error) {
+    return [];
+  }
+});
 
 // Crea un nuevo slice (característica)
 const cartSlice = createSlice({
@@ -37,21 +56,45 @@ const cartSlice = createSlice({
       // Decrementa la cantidad del item
       cartItem.amount--;
     },
-    calculateTotals:(state)=>{
-      let amount=0;
-      let total=0;
-       state.cartItems.forEach(item=>{
-        amount+=item.amount;
-        total+=item.amount*item.price
-       })
-      //modifica el estado
-       state.amount=amount;
-       state.total=total;
-    }
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      // Recorre los items para calcular el amount y total del carrito
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      // Modifica el estado
+      state.amount = amount;
+      state.total = total;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      // Petición pendiente
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true;
+      })
+      // Petición exitosa
+      .addCase(getCartItems.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        // Modifica el estado.
+        // payload = datos obtenidos del backend
+        state.cartItems = payload;
+      })
+      // Petición fallida
+      .addCase(getCartItems.rejected, (state) => {
+        state.isLoading = false;
+      });
   },
 });
 
-export const { clearCart, removeItem, increaseItemAmount, decreaseItemAmount,calculateTotals } =
-  cartSlice.actions;
+export const {
+  clearCart,
+  removeItem,
+  increaseItemAmount,
+  decreaseItemAmount,
+  calculateTotals,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
